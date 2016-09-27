e85aab4eccd5ee3addaddbb3ba7042dfcc5cb3ad
Reuben D'Netto <rdnetto@atlassian.com>
2016-09-27 19:33:59 +1000

    Fixed white screen when run on 1920x1200

--- lock.sh
+++ lock.sh
@@ -1,10 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
 OLD=$(tempfile)
 xrandr > $OLD
+RES=$(awk '/[*]/{print $1}' $OLD | head -n1)
+
+# Next doesn't have a version in this size
+if [ "$RES" == "1920x1200" ]; then
+    RES="1920x1080"
+fi
 
 # lock screen
-i3lock -n -e -f -t -i /usr/share/wallpapers/Next/contents/images/$(awk '/[*]/{print $1}' $OLD | head -n1).png
+i3lock -n -e -f -t -i /usr/share/wallpapers/Next/contents/images/$RES.png
 
 NEW=$(tempfile)
 xrandr > $NEW
