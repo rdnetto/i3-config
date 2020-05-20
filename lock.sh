6ddb87a9da576140c7053905e91497ab780f5094
Renee D'Netto <rdnetto@atlassian.com>
2020-05-20 21:42:43 +1000

    Use wallpaper for lock screen

--- lock.sh
+++ lock.sh
@@ -2,21 +2,15 @@
 
 OLD=$(tempfile)
 xrandr > $OLD
-RES=$(awk '/[*]/{print $1}' $OLD | head -n1)
-
-# Next doesn't have a version in this size
-if [ "$RES" == "1920x1200" ]; then
-    RES="1920x1080"
-fi
 
 # lock screen
-i3lock -n -e -f -t -i /usr/share/wallpapers/Next/contents/images/$RES.png
+i3lock -n -e -f -t -i /home/renee/.config/i3/wallpapers/transbianSky.png
 
 NEW=$(tempfile)
 xrandr > $NEW
 
 echo $OLD
 echo $NEW
-cmp -s $OLD $NEW || /home/reuben/bin/toggle-screens.sh
+cmp -s $OLD $NEW || /home/renee/bin/toggle-screens.sh
 rm $OLD $NEW
 
